87faecb5876dd83de2bca8d5b419df3ee63363c4
Ray Ehrhardt <ray.ehrhardt@gmail.com>
2021-08-08 17:34:21 +0200

    Fixed weird issue with suddenly failing tests: Booting the kernel before calling "Symfony\Bundle\FrameworkBundle\Test\WebTestCase::createClient()" is not supported, the kernel should only be booted once

--- tests/Controller/AccountControllerTest.php
+++ tests/Controller/AccountControllerTest.php
@@ -8,6 +8,11 @@ class AccountControllerTest extends WebTestCase
 {
     public function testSuccessfulResponseOfIndexAction(): void
     {
+        // TODO: i do not know why this is necessary...
+        // TODO: Suddenly this tes kept failing with:
+        // TODO: Booting the kernel before calling "Symfony\Bundle\FrameworkBundle\Test\WebTestCase::createClient()" is not supported, the kernel should only be booted once
+        static::$booted = false;
+
         $client = static::createClient();
         $crawler = $client->request('GET', '/account/');
 
